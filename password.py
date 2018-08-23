7e9e5cecabe252cdd5ef385d82247fcac2d03081
EugeneZnm <eugenenzioki@gmail.com>
2018-08-23 22:21:16 +0300

    creating instance of Userinfo

--- password.py
+++ password.py
@@ -2,5 +2,14 @@ class Userinfo:
     # 1.1 class variable to store created objects
     user_details = []
 
+    # 1.2 creating instances of Userinfo
+    def __init__(self, first_name, last_name, email):
+
+        # define properties for objects
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+
 
 
