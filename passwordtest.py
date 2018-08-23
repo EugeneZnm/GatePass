b6ac6b1a4ffdcf1e6cc06aeb5a9a32f5c7394656
EugeneZnm <eugenenzioki@gmail.com>
2018-08-23 23:28:30 +0300

    fix class importation, test for proper object initialisation

--- passwordtest.py
+++ passwordtest.py
@@ -4,7 +4,7 @@ import unittest
 
 # 2 import Userinfo class
 
-from userinfo import Userinfo
+from password import Userinfo
 
 
 # 3 subclass TestUserinfo inheriting from unittest.Testcase
@@ -14,5 +14,14 @@ class TestUserinfo(unittest.TestCase):
     def setUp(self):
 
         # 5 creating new Userinfo object
-        self.new_userinfo = Userinfo("", "", "")
+        self.new_userinfo = Userinfo("Genghis", "Khan", "chingis@gmail.com")
+
+    # 6 Test for proper object initialisation
+    def test__init(self):
+        self.assertEqual(self.new_userinfo.first_name, "Genghis")
+        self.assertEqual(self.new_userinfo.last_name, "Khan")
+        self.assertEqual(self.new_userinfo.email, "chingis@gmail.com")
+
+if __name__ == '__main__':
+    unittest.main()
 
