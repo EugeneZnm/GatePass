f1d901b3b6b5c3809da1dca7d246480fad0a0240
EugeneZnm <eugenenzioki@gmail.com>
2018-08-24 11:07:10 +0300

    adding passkey object

--- password.py
+++ password.py
@@ -3,12 +3,12 @@ class Userinfo:
     user_details = []
 
     # 1.2 method creating instances of Userinfo
-    def __init__(self, first_name, last_name, email):
+    def __init__(self, first_name, last_name, passkey):
 
         # define properties for objects
         self.first_name = first_name
         self.last_name = last_name
-        self.email = email
+        self.passkey = passkey
 
     # 1.3 method saving user info objects into user details list
     def save_details(self):
